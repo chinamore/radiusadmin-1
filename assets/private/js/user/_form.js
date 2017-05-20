8788b7378d229b7821c8adb34d67a8a6e170a20a
Paulo Dias <prmdjweb@gmail.com>
2017-05-20 22:08:19 +0000

    Altered js for add group in view user create/update - Not allow 2 equal groups

--- assets/private/js/user/_form.js
+++ assets/private/js/user/_form.js
@@ -7,13 +7,23 @@ $("#input-add-group").autocompleter({
 $("#btn-add-group").on( "click", function() { 
 
     var group = $("#input-add-group").val();
+    
+    $("#input-add-group").val("");
 
-    if( $.trim( group ).length > 0  ) {
+    if( $.trim( group ).length <= 0 ) {
+        
+        return;
+    }
+
+    var groups = $.map( $("#select-groups option") ,function(option) {
+        return option.value;
+    });
+
+    if( $.inArray( group, groups ) === -1) {
         
         $("#select-groups").append( "<option value='" + group + "'>" + group + "</option>" );
-        
-        $("#input-add-group").val("");
-
+    
         $("#input-add-group").focus();
     }
+
 });
