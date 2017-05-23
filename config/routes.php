5f0330d6c3b376fef546325465347115f12cd4c9
Paulo Dias <prmdjweb@gmail.com>
2017-05-23 15:49:54 +0000

    Translated the routes to english in config/routes

--- config/routes.php
+++ config/routes.php
@@ -2,45 +2,45 @@
 
 $app->map( ["GET"], "/", App\Controller\Radius\AppController::class . ":actionIndex" );
 
-$app->group("/usuarios", function () {
+$app->group("/users", function () {
 
-    $this->map( ["GET"], "/[listar]", App\Controller\Radius\UserController::class . ":actionList" );
-    $this->map( ["POST"], "/armazenar", App\Controller\Radius\UserController::class . ":actionStore" );
+    $this->map( ["GET"], "/[list]", App\Controller\Radius\UserController::class . ":actionList" );
+    $this->map( ["POST"], "/store", App\Controller\Radius\UserController::class . ":actionStore" );
     
-    $this->map( ["GET", "POST"], "/criar", App\Controller\Radius\UserController::class . ":actionCreate" );
-    $this->map( ["GET", "POST"], "/alterar", App\Controller\Radius\UserController::class . ":actionUpdate" );
-    $this->map( ["GET", "POST"], "/apagar", App\Controller\Radius\UserController::class . ":actionDelete" );
-    $this->map( ["GET", "POST"], "/estatistica", App\Controller\Radius\UserController::class . ":actionStatistic" );
+    $this->map( ["GET", "POST"], "/create", App\Controller\Radius\UserController::class . ":actionCreate" );
+    $this->map( ["GET", "POST"], "/update", App\Controller\Radius\UserController::class . ":actionUpdate" );
+    $this->map( ["GET", "POST"], "/delete", App\Controller\Radius\UserController::class . ":actionDelete" );
+    $this->map( ["GET", "POST"], "/statistics", App\Controller\Radius\UserController::class . ":actionStatistic" );
 });
 
-$app->group("/grupos", function () {
+$app->group("/groups", function () {
 
-    $this->map( ["GET"], "/[listar]", App\Controller\Radius\GroupController::class . ":actionList" );
+    $this->map( ["GET"], "/[list]", App\Controller\Radius\GroupController::class . ":actionList" );
 
-    $this->map( ["GET", "POST"], "/criar", App\Controller\Radius\GroupController::class . ":actionCreate" );
-    $this->map( ["GET", "POST"], "/alterar", App\Controller\Radius\GroupController::class . ":actionUpdate" );
-    $this->map( ["GET", "POST"], "/apagar", App\Controller\Radius\GroupController::class . ":actionDelete" );
+    $this->map( ["GET", "POST"], "/create", App\Controller\Radius\GroupController::class . ":actionCreate" );
+    $this->map( ["GET", "POST"], "/update", App\Controller\Radius\GroupController::class . ":actionUpdate" );
+    $this->map( ["GET", "POST"], "/delete", App\Controller\Radius\GroupController::class . ":actionDelete" );
 });
 
-$app->group("/estatisticas", function () {
+$app->group("/statistics", function () {
 
-    $this->map( ["GET"], "/[listar]", App\Controller\Radius\StatisticController::class . ":actionList" );
+    $this->map( ["GET"], "/[list]", App\Controller\Radius\StatisticController::class . ":actionList" );
 });
 
-$app->group("/clientes", function () {
+$app->group("/clients", function () {
 
-    $this->map( ["GET"], "/[listar]", App\Controller\Radius\ClientController::class . ":actionList" );
-    $this->map( ["POST"], "/armazenar", App\Controller\Radius\ClientController::class . ":actionStore" );
+    $this->map( ["GET"], "/[list]", App\Controller\Radius\ClientController::class . ":actionList" );
+    $this->map( ["POST"], "/store", App\Controller\Radius\ClientController::class . ":actionStore" );
 
-    $this->map( ["GET", "POST"], "/criar", App\Controller\Radius\ClientController::class . ":actionCreate" );
-    $this->map( ["GET", "POST"], "/alterar", App\Controller\Radius\ClientController::class . ":actionUpdate" );
-    $this->map( ["GET", "POST"], "/apagar", App\Controller\Radius\ClientController::class . ":actionDelete" );
+    $this->map( ["GET", "POST"], "/create", App\Controller\Radius\ClientController::class . ":actionCreate" );
+    $this->map( ["GET", "POST"], "/update", App\Controller\Radius\ClientController::class . ":actionUpdate" );
+    $this->map( ["GET", "POST"], "/delete", App\Controller\Radius\ClientController::class . ":actionDelete" );
 });
 
 $app->group("/json", function () {
 
-    $this->group("/grupos", function () {
-        $this->map( ["GET", "POST"], "/[listar]", App\Controller\Radius\GroupController::class . ":actionListJSON" );
-        $this->map( ["GET", "POST"], "/existe", App\Controller\Radius\GroupController::class . ":actionExistJSON" );
+    $this->group("/groups", function () {
+        $this->map( ["GET", "POST"], "/[list]", App\Controller\Radius\GroupController::class . ":actionListJSON" );
+        $this->map( ["GET", "POST"], "/exist", App\Controller\Radius\GroupController::class . ":actionExistJSON" );
     });
 });
