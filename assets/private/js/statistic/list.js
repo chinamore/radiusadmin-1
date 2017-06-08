11f291805ecaa89a49d7ad4f600be5750d7803e4
Paulo Dias <prmdjweb@gmail.com>
2017-06-08 19:10:37 +0000

    Improved Statistic list

--- assets/private/js/statistic/list.js
+++ assets/private/js/statistic/list.js
@@ -1,72 +1,86 @@
-var data = {
-    labels: ["01/01/2017", "02/01/2017", "03/01/2017", "04/01/2017", "05/01/2017"],
-    datasets: [
-        {
-            label: "Total de conexões",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "#49739C",
-            borderColor: "#49739C",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56],// 55, 40, 32, 41, 70],
-            spanGaps: false,
-        },
 
-        {
-            label: "Total de usuários",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "#D75553",
-            borderColor: "#D75553",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [20, 78, 22, 40, 32],//, 41, 70, 56, 55, 40],
-            spanGaps: false,
+function createSummaryConnectionsChart( dates, connections, users ) {
+
+    var data = {
+        labels: dates,
+        datasets: [
+            {
+                label: "Total de conexões",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "#49739C",
+                borderColor: "#49739C",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: connections,
+                spanGaps: false,
+            },
+             {
+                label: "Total de usuários",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "#D75553",
+                borderColor: "#D75553",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: users,
+                spanGaps: false,
+            }
+           
+        ]
+    };
+
+    var ctx = $("#canvas-chart-connection");
+
+    var chartSummaryConnections = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            
+            animation: { 
+                animateScale: true
+            },
+            
+            responsive: true,
+            
+            tooltips: {
+                enabled: false
+            }, 
+            
+            legend:{
+                position: "bottom"
+            }
         }
+    });
+}
 
-    ]
-};
+
+
+
                 
 
-var ctx = $("#canvas-chart-connection");
-
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-        data: data,
-        options:{animation:{animateScale:!0},responsive:!0,tooltips:{enabled:!1}, legend:{position: 'bottom'}}
-});
-
-
-var ctx = $("#canvas-chart-connection2");
-
-var myLineChart2 = new Chart(ctx, {
-    type: 'line',
-        data: data,
-        options:{animation:{animateScale:!0},responsive:!0,tooltips:{enabled:!1}, legend:{position: 'bottom'}}
-});
 
 
         var data = {
--- src/Controller/Radius/StatisticController.php
+++ src/Controller/Radius/StatisticController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Radius;
 
 use App\Controller\Controller;
+use App\Model\Radius\RadAcct;
+use App\Model\Radius\RadPostAuth;
 use \DateTime;
 use \DateInterval;
 
@@ -13,46 +15,191 @@ class StatisticController extends Controller {
         parent::__construct( $container );
     }
 
-    public function actionList( $request, $response ) {
+    public function actionList( $request, $response ) { 
 
+        
+        $textDate1 = $request->getQueryParam( "date1", date( "01/01/Y" ) );
 
-        return $this->view->render( $response, "Radius/Statistic/list.html");
+        $textDate2 = $request->getQueryParam( "date2", date( "d/m/Y" ) );
+        
+        $date1 = Datetime::createFromFormat( "d/m/Y", $textDate1 );
+        $date2 = Datetime::createFromFormat( "d/m/Y", $textDate2 );
+
+        $datesTextSummary = $this->getDatesSummary( $date1, $date2 );
+
+        $connectionsSummary = [];
+        $usersSummary = [];
+
+        foreach( $datesTextSummary as $date ) {
+             
+            $summaryDate1 = Datetime::createFromFormat( "d/m/Y", $date );
+
+            $summaryDate2 = clone $summaryDate1;
+            
+            $summaryDate2->add( new DateInterval( "P1D" ) );
+
+            $connectionsSummary[] = $this->countConnections( $summaryDate1, $summaryDate2 );
+            $usersSummary[] = $this->countUsers( $summaryDate1, $summaryDate2 );
+        }
+
+        $lastAuthsOk = $this->getAuthsOk( $date1, $date2, 0, 5 );
+        
+        $lastAuthsError = $this->getAuthsError( $date1, $date2, 0, 5 );
+        
+        $qtUsers = $this->countUsers( $date1, $date2 );
+
+        $qtAccts = $this->countConnections( $date1, $date2 );
+
+        $timeAvg = $this->avgTimeConnections( $date1, $date2 );
+
+        $uploadAvg = $this->avgUploadConnections( $date1, $date2 );
+
+        $downloadAvg = $this->avgDownloadConnections( $date1, $date2 );
+      
+        return $this->view->render( $response, "Radius/Statistic/list.html", [
+           
+            "date1"=>$textDate1,
+            "date2"=>$textDate2,
+            "qtAccts"=>$qtAccts,
+            "qtUsers"=>$qtUsers,
+            "timeAvg"=>$timeAvg,
+            "uploadAvg"=>$uploadAvg,
+            "downloadAvg"=>$downloadAvg,
+            "datesTextSummary"=>$datesTextSummary,
+            "connectionsSummary"=>$connectionsSummary,
+            "usersSummary"=>$usersSummary,
+            "lastAuthsOk"=>$lastAuthsOk,
+            "lastAuthsError"=>$lastAuthsError
+        ]);
     }
 
-    private function getDatesPointsToChart( $date1, $date2 ) {
+    private function getDatesSummary( $date1, $date2 ) {
     
         $days = [];
         
         $date1Work = clone $date1;
             
-        $dayYear1 = $date1->format("z");
-        $dayYear2 = $date2->format("z");
-
-        if( ( $dayYear2 - $dayYear1 ) >= 9  ) {
-
-            $increment = round( ( $dayYear2 - $dayYear1 ) / 9 ) ;
+        $dayYear1 = (int) $date1->format( "z" );
+        $dayYear2 = (int) $date2->format( "z" );
+        
+        $qtDays = $dayYear2 - $dayYear1;
+        
+        if( $qtDays == 0 || $qtDays == 1 ) {
             
-            $days[] = $date1Work->format("Y-m-d");
+            $date1Work->sub( new DateInterval( "P1D" ) );
+            $days[] = $date1Work->format( "d/m/Y" );
 
-            for( $i = 0; $i < 8; $i++ ) {
-        
-                $date1Work->add( new DateInterval( "P${increment}D"));
-            
-                $days[] = $date1Work->format("Y-m-d");
-            }
 
-            $days[] = $date2->format("Y-m-d");
+            $date1Work->add( new DateInterval( "P1D" ) );
+            $days[] = $date1Work->format( "d/m/Y" );
+
+
+            $date1Work->add( new DateInterval( "P1D" ) );
+            $days[] = $date1Work->format( "d/m/Y" );
+
+            return $days;
+        }    
         
-        }else {
+        if( $qtDays < 9 ) {
         
-            for(; $dayYear1 <= $dayYear2; $dayYear1++ ) {
+             for(; $dayYear1 <= $dayYear2; $dayYear1++ ) {
                                 
-                $days[] = $date1Work->format("Y-m-d");
+                $days[] = $date1Work->format( "d/m/Y" );
   
-                $date1Work->add( new DateInterval( "P1D"));
+                $date1Work->add( new DateInterval( "P1D") );
             }
+        
+            return $days;
         }
 
+        
+        $increment = round( ( $dayYear2 - $dayYear1 ) / 9 ) ;
+            
+        $days[] = $date1Work->format( "d/m/Y" );
+
+        for( $i = 0; $i < 8; $i++ ) {
+        
+            $date1Work->add( new DateInterval( "P${increment}D") );
+            
+            $days[] = $date1Work->format( "d/m/Y" );
+        }
+
+        $days[] = $date2->format( "d/m/Y" );
+        
         return $days;
     }
+
+    private function countConnections( $date1, $date2 ) {
+       
+        return RadAcct::where( "acctstarttime", ">=", $date1->format( "Y-m-d" ) )
+            ->where( "acctstarttime", "<", $date2->format( "Y-m-d" ) )
+            ->count();
+
+    }
+
+    private function countUsers( $date1, $date2 ) {
+        
+         //is this a better way?
+        $raw = RadAcct::where( "acctstarttime", ">=", $date1->format( "Y-m-d" ) )
+            ->where( "acctstarttime", "<", $date2->format( "Y-m-d" ) )
+            ->selectRaw( "count( distinct username ) as qtUsers")
+            ->first();
+    
+        return $raw->qtUsers;
+     
+     }
+
+    private function avgTimeConnections( $date1, $date2 ) {
+
+        return RadAcct::where( "acctstarttime", ">=", $date1->format( "Y-m-d" ) )
+            ->where( "acctstarttime", "<", $date2->format( "Y-m-d" ) )
+            ->avg( "acctsessiontime" );
+     }
+
+     private function avgUploadConnections( $date1, $date2 ) {
+
+        return RadAcct::where( "acctstarttime", ">=", $date1->format( "Y-m-d" ) )
+            ->where( "acctstarttime", "<", $date2->format( "Y-m-d" ) )
+            ->avg( "acctinputoctets" );
+     }
+
+     private function avgDownloadConnections( $date1, $date2 ) {
+
+        return RadAcct::where( "acctstarttime", ">=", $date1->format( "Y-m-d" ) )
+            ->where( "acctstarttime", "<", $date2->format( "Y-m-d" ) )
+            ->avg( "acctoutputoctets" );
+     }
+
+    private function getAuthsOk( $date1, $date2, $skip = null , $take = null ) {
+    
+        $query = RadPostAuth::where( "authdate", ">=", $date1->format( "Y-m-d" ) )
+            ->where( "authdate", "<", $date2->format( "Y-m-d" ) )
+            ->where( "reply", "like", "%accept%" )
+            ->orderBy( "id", "desc");
+            
+        if( $skip !== null )     
+            $query->skip( $skip );
+
+        if( $take !== null )
+            $query->take( $take );
+
+        return $query->get();
+    }
+
+    private function getAuthsError( $date1, $date2, $skip = null , $take = null ) {
+    
+        $query = RadPostAuth::where( "authdate", ">=", $date1->format( "Y-m-d" ) )
+            ->where( "authdate", "<", $date2->format( "Y-m-d" ) )
+            ->where( "reply", "like", "%reject%" )
+            ->orderBy( "id", "desc");
+            
+        if( $skip !== null )     
+            $query->skip( $skip );
+
+        if( $take !== null )
+            $query->take( $take );
+
+        return $query->get();
+    }
+
 }
